4a8392732cb232daba55c6c1ef66b5f05d41da77
Pavanipinky102 <pavanik418@gmail.com>
2025-05-14 13:52:14 +0530

    Create single array.c

---
+++ single array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int a;
+int pav[2][3]={{4,5,3}};
+int main(){ 
+    for(a=0;a<2;a++){
+            printf("%d\n",pav[a]);
+                return 0;
+            }
+            
+    }
